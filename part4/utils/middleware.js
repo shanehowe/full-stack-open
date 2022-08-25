3dd7e832e87604d58a2ace39d8f542a0b9582b71
Shane Howe <shanehowe100@hotmail.com>
2022-08-25 10:45:40 +0100

    merge

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -19,7 +19,7 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError')
     {
         return response.status(400).send(
-            { error: 'malformatted id'}
+            { error: 'malformatted id' }
         )
     } else if (error.name === 'ValidationError')
     {
