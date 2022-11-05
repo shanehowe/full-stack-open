e5c6cb5e8342f562656476eee9b46e5b493fd47f
Shane Howe <shanehowe100@hotmail.com>
2022-11-05 21:56:50 +0000

    helper function for testing

---
+++ part4/tests/api_helper.js
@@ -0,0 +1,12 @@
+// Helper module for testing API
+
+const User = require('../models/user');
+
+const usersInDb = async () => {
+    const users = await User.find({});
+    return users.map(u => u.toJSON());
+}
+
+module.exports = {
+    usersInDb
+};
