0984cdee8ec728dc3de895847812e0bce9e56098
Shane Howe <shanehowe100@hotmail.com>
2022-11-05 22:39:04 +0000

    added validation checks when creating new user

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -11,6 +11,29 @@ usersRouter.get('/api/users', async (request, response) => {
 usersRouter.post('/api/users', async (request, response) => {
     const { username, name, password } = request.body;
 
+    // TODO: validate username and password
+    // Both username and password must be at least 3 characters long
+    // username must be unique
+    if (!username || !password) {
+        return response.status(400).json({
+            error: 'username and password must be provided'
+        });
+    }
+    
+    if (password.length < 3 || username.length < 3) {
+        return response.status(400).json({ 
+            error: 'password and username must be at least 3 characters long' 
+        });
+    }
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+        return response.status(400).json({ 
+            error: 'username must be unique' 
+        });
+    }
+
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(password, saltRounds);
 
