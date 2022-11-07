6778b20e4ca53779ad09aef0337a80ad8bf9b471
Shane Howe <shanehowe100@hotmail.com>
2022-11-07 17:55:29 +0000

    get route added for getting specific user

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -3,9 +3,21 @@ const usersRouter = require('express').Router();
 const User = require('../models/user');
 
 usersRouter.get('/api/users', async (request, response) => {
-    const users = await User.find({});
+    const users = await User
+        .find({})
+        .populate('blogs', { url: 1, title: 1, author: 1 });
 
     response.status(200).json(users);
+});
+
+usersRouter.get('/api/users/:id', async (request, response) => {
+    const user = await User.findById(request.params.id);
+
+    if (user) {
+        response.status(200).json(user);
+    } else {
+        response.status(404).end();
+    }
 });
 
 usersRouter.post('/api/users', async (request, response) => {
@@ -14,7 +26,7 @@ usersRouter.post('/api/users', async (request, response) => {
     // TODO: validate username and password
     // Both username and password must be at least 3 characters long
     // username must be unique
-    if (!username || !password) {
+    if (!(username && password)) {
         return response.status(400).json({
             error: 'username and password must be provided'
         });
